9a0a9d4528ea508be066e745e66167a88b9dbbb7
Noah-Canned-Door <114012463+Noah-Canned-Door@users.noreply.github.com>
2022-09-28 15:35:36 +0100

    Added timer

--- Scenes/Timer_Display.gd
+++ Scenes/Timer_Display.gd
@@ -3,12 +3,17 @@ var ms = 0
 var s = 0
 var m = 0
 
+
 func _process(delta):
-	if ms > 9:
+	if ms > 99:
 		s += 1
 		ms = 0
 	if s > 59:
 		s = 0
 		m +=1
-	set_text(str(m)+":"+str(s)+":"+str(ms)+":")
+	set_text(str(m)+":"+ str("%02d" %s)+":"+str(ms))
 	pass	
+
+
+func _on_Timer_timeout():
+	ms += 1
