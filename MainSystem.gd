f628c4b9819355edac46a00c8caa2042a66b44e0
Noah-Canned-Door <114012463+Noah-Canned-Door@users.noreply.github.com>
2022-10-13 16:36:32 +0100

    Database can now delete rows

--- MainSystem.gd
+++ MainSystem.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 const SQLite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
 var db
@@ -7,9 +7,7 @@ var db_name = "res://DataStore/database"
 func _ready():
 	db = SQLite.new()
 	db.path = db_name
-	var  item_Result = getItemsByUserID(1)
-	commitDataToDB()
-	readFromDB()
+	var item_Result = getItemsByUserID(1)
 	pass
 
 func commitDataToDB():
@@ -18,6 +16,7 @@ func commitDataToDB():
 	var dict : Dictionary = Dictionary()
 	dict["UserName"] = "Octowhat"
 	dict["Time"] = 105327
+	dict["MapID"] = 1
 	
 	db.insert_row(TableName,dict)
 
@@ -26,11 +25,16 @@ func readFromDB():
 	var TableName = "Player_Info"
 	db.query("select * from " + TableName + ";")
 	for i in range(0,db.query_result.size()):
-		print("Query results ",db.query_result[i]["UserName"],db.query_result[i]["Time"] )
+		print("Query results ",db.query_result[i]["ID"],db.query_result[i]["UserName"],db.query_result[i]["Time"] )
 
 func getItemsByUserID(id):
 	db.open_db()
-	db.query("select Player_Info.UserName as Uname,Player_Info.Time as Time,Map_info.Name as Mname from Player_Info INNER join Map_info on Player_Info.ID = Map_info.PlayerID where Player_Info.ID = " + str(id))
+	db.query("SELECT * from Player_Info INNER join Map_info on Player_Info.MapID = Map_info.ID= " + str(id))
 	for i in range(0,db.query_result.size()):
-		print("Query results ",db.query_result[i]["Uname"],db.query_result[i]["Time"],db.query_result[i]["Mname"])
+		print("Query results ",db.query_result[i]["UserName"],db.query_result[i]["Time"],db.query_result[i]["Name"])
 	return db.query_result
+
+func removeDataFromDB():
+	db.open_db()
+	db.query("DELETE from Player_Info WHERE Player_Info.UserName = '" + "Octowhat" + "'")
+	
--- Scenes/Start.gd
+++ Scenes/Start.gd
@@ -7,3 +7,8 @@ func _on_Start_pressed():
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+
+func _on_Account_pressed():
+	get_tree().change_scene("res://Scenes/Login_screen.tscn")
+	pass # Replace with function body.
